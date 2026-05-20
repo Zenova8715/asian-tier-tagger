09757ab92b6d5ea6fff5358f49d4602d3543bd60
Zenova8715 <voidfury1527@gmail.com>
2026-05-20 16:52:05 +0530

    Update ConfigScreen.java

--- src/main/java/com/asiantiertagger/config/ConfigScreen.java
+++ src/main/java/com/asiantiertagger/config/ConfigScreen.java
@@ -149,7 +149,7 @@ public class ConfigScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        renderBackground(context);
+        renderBackground(context, mouseX, mouseY, delta);
         super.render(context, mouseX, mouseY, delta);
 
         // Title
